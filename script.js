a2a45ef6dde02c3113453a834c2f95726a8db405
annalene97 <annalenerasmussen@outlook.com>
2024-02-27 13:54:25 +0100

    script

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-alert("Hello world!");
+const filterKnapper = document.querySelectorAll(".knap button");
+const retter = document.querySelectorAll(".retter-grid .ret");
+
+const filterRetter = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+    /* retter.forEach(ret => {
+        ret.classList.add("hidden");
+
+        if (ret.dataset.name === e.target.dataset.name || e.target.dataset.name === "alle") {
+            ret.classList.remove("hidden");
+        }
+    }) */
+
+    retter.forEach(ret => {
+        ret.classList.add("hidden");
+
+        const filter = ret.dataset.name.split(','); // Split the categories of the recipe into an array
+
+        if (filter.includes(e.target.dataset.name) || e.target.dataset.name === "alle") {
+            ret.classList.remove("hidden");
+        }
+    })
+}
+
+filterKnapper.forEach(button => button.addEventListener("click", filterRetter));
+
+
+/* function filtreretRetter(c){
+    var x, i;
+    x = document.getElementsByClassName("ret");
+    if (c == "all")
+}
+ */
+
+
+
+
+console.log("hi");
